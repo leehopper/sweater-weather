8905b8d857520ab618f0e914d86423c2b8ab7c2c
Lee Hopper <hopper.lee.j@gmail.com>
2021-09-25 15:35:22 -0600

    Build route for get forecast

---
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'Forecast API' do
+  describe 'return weather for a city' do
+    it 'returns json given a city' do
+      get '/api/v1/forecast?location=denver,co'
+
+      expect(response).to be_successful
+    end
+  end
+end
