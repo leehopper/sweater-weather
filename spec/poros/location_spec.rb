7b2e8a5b52813fe009abe22fb84b9f20901fba52
Lee Hopper <hopper.lee.j@gmail.com>
2021-09-25 19:03:23 -0600

    Build test for location poros

---
+++ spec/poros/location_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe Location, :vcr do
+  before(:each) do
+    json = MapQuestService.get_coordinates('Denver,co')
+    @location = Location.new(json)
+  end
+
+  it 'exists' do
+    expect(@location).to be_a(Location)
+  end
+
+  it 'attributes' do
+    expect(@location.lat).to eq(39.738453)
+    expect(@location.long).to eq(-104.984853)
+  end
+end
