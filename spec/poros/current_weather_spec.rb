274e0f96c760f35029893209fe7159130f0da6df
Lee Hopper <hopper.lee.j@gmail.com>
2021-09-25 19:17:25 -0600

    Build testing for current weather poros

--- spec/poros/current_weather_spec.rb
+++ spec/poros/current_weather_spec.rb
@@ -1,12 +1,29 @@
-# expect(@forecast.conditions).to be_a(String)
-# expect(@forecast.datetime).to be_a(DateTime)
-# expect(@forecast.feels_like).to be_a(Float)
-# expect(@forecast.humidity).to be_a(Integer)
-# expect(@forecast.icon).to be_a(String)
-# expect(@forecast.sunrise).to be_a(DateTime)
-# expect(@forecast.sunset).to be_a(DateTime)
-# expect(@forecast.temperature).to be_a(Float)
-# expect(@forecast.uvi).to be_a(Integer)
-# expect(@forecast.visibility).to be_a(Integer)
-# expect(@forecast.daily_weather).to eq([])
-# expect(@forecast.hourly_weather).to eq([])
+require 'rails_helper'
+
+describe CurrentWeather, :vcr do
+  before(:each) do
+    location = double()
+    allow(location).to receive(:lat).and_return(39.738453)
+    allow(location).to receive(:long).and_return(-104.984853)
+
+    json = OpenWeatherService.get_weather(location)
+    @current_weather = CurrentWeather.new(json[:current])
+  end
+
+  it 'exists' do
+    expect(@current_weather).to be_a(CurrentWeather)
+  end
+
+  it 'attributes' do
+    expect(@current_weather.conditions).to be_a(String)
+    expect(@current_weather.datetime).to be_a(Time)
+    expect(@current_weather.feels_like).to be_a(Float)
+    expect(@current_weather.humidity).to be_a(Integer)
+    expect(@current_weather.icon).to be_a(String)
+    expect(@current_weather.sunrise).to be_a(Time)
+    expect(@current_weather.sunset).to be_a(Time)
+    expect(@current_weather.temperature).to be_a(Float)
+    expect(@current_weather.uvi).to be_a(Integer)
+    expect(@current_weather.visibility).to be_a(Integer)
+  end
+end
