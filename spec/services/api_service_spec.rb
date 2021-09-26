72708499223e5ca4735e395c3c862a3a1b860690
Lee Hopper <hopper.lee.j@gmail.com>
2021-09-25 18:43:55 -0600

    Build testing for api service

---
+++ spec/services/api_service_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'ApiServiceSpec', :vcr do
+  it 'returns parsed json with symbolized names' do
+    response = Faraday.new('http://www.mapquestapi.com/geocoding/v1/address').get do |f|
+      f.params['key'] = ENV['map_quest_key']
+      f.params['location'] = 'Denver,co'
+    end
+
+    expected = ApiService.get_json(response)
+
+    expect(expected).to be_a(Hash)
+    expect(expected).to have_key(:results)
+    expect(expected[:results][0]).to have_key(:locations)
+    expect(expected).to have_key(:results)
+  end
+end
