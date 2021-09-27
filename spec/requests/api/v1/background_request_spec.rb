c47685f3bb6d22fca64e501d1967e1298b893cf1
Lee Hopper <hopper.lee.j@gmail.com>
2021-09-27 14:20:07 -0600

    Build out testing for json response

--- spec/requests/api/v1/background_request_spec.rb
+++ spec/requests/api/v1/background_request_spec.rb
@@ -11,6 +11,23 @@ describe 'Background API', :vcr do
 
       bg = JSON.parse(response.body, symbolize_names: true)[:data]
 
+      expect(bg).to have_key(:id)
+      expect(bg[:id]).to eq(nil)
+      expect(bg).to have_key(:type)
+      expect(bg[:type]).to eq('image')
+
+      attributes = bg[:attributes]
+
+      expect(attributes).to have_key(:location)
+      expect(attributes[:location]).to be_a(String)
+      expect(attributes).to have_key(:image_url)
+      expect(attributes[:image_url]).to be_a(String)
+      expect(attributes).to have_key(:source)
+      expect(attributes[:source]).to be_a(String)
+      expect(attributes).to have_key(:photographer)
+      expect(attributes[:photographer]).to be_a(String)
+      expect(attributes).to have_key(:photographer_profile)
+      expect(attributes[:photographer_profile]).to be_a(String)
     end
   end
 end
