6adf6297d4017b543179f5f3c74bb061d02d4b49
Lee Hopper <hopper.lee.j@gmail.com>
2021-09-28 19:12:39 -0600

    Build test for invalid destination sad path and record cassette

--- spec/requests/api/v1/road_trips/road_trip_request_spec.rb
+++ spec/requests/api/v1/road_trips/road_trip_request_spec.rb
@@ -64,5 +64,39 @@ describe 'road trips', :vcr do
       expect(error[:errors][0][:title]).to eq('Unauthorized')
       expect(error[:errors][0][:message]).to eq('Invalid api key')
     end
+
+    it 'returns error message for impossible destination' do
+      user = create(:user, :with_api_key)
+
+      post_params = {
+        origin: 'denver,co',
+        destination: 'london,uk',
+        api_key: user.api_keys.first.token
+      }
+
+      headers = { 'CONTENT_TYPE' => 'application/json' }
+
+      post '/api/v1/road_trip', headers: headers, params: JSON.generate(body: post_params)
+
+      expect(response).to be_successful
+
+      rt = JSON.parse(response.body, symbolize_names: true)[:data]
+
+      expect(rt).to have_key(:id)
+      expect(rt[:id]).to eq(nil)
+      expect(rt).to have_key(:type)
+      expect(rt[:type]).to eq('roadtrip')
+
+      details = rt[:attributes]
+
+      expect(details).to have_key(:start_city)
+      expect(details[:start_city]).to eq('denver,co')
+      expect(details).to have_key(:end_city)
+      expect(details[:end_city]).to eq('london,uk')
+      expect(details).to have_key(:travel_time)
+      expect(details[:travel_time]).to eq('impossible')
+      expect(details).to have_key(:weather_at_eta)
+      expect(details[:weather_at_eta]).to eq('')
+    end
   end
 end
